6a32f8c1b2f64d150e526450f03d619b0e7b9634
Agustí Grau <agusti@lacalafa.cat>
2024-07-15 15:14:30 +0200

    Create project_changed_names.ps1

---
+++ project_changed_names.ps1
@@ -0,0 +1,29 @@
+# Variables
+$beacon = "your_pat"
+$organization = "your_organization"
+$subject = "Agusti Grau provides this code"
+$emailTo = "target address"
+$emailFrom = "origina address"
+$smtpServer = "smtp server"
+
+$url = "https://dev.azure.com/$organization/_apis/projects?api-version=6.0"
+$projects = Invoke-RestMethod -Uri $url -Method Get -Headers @{Authorization = "Basic $([Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$beacon")))"}
+$renamedProjects = @()
+foreach ($project in $projects) {
+        # $lastUpdateTime = [datetime]::ParseExact($project.lastUpdateTime, $format,  $cultureInfo)
+        $lastUpdateTime = $project.lastUpdateTime
+        if ((Get-Date) - $lastUpdateTime -lt (New-TimeSpan -Days 1)) {
+            $renamedProjects += $project
+            Write-Host $project.name + "has been updated"
+        }
+    }
+if ($renamedProjects.Count -gt 0) {
+        $body = "The following projects have been renamed:`n`n"
+    foreach ($project in $renamedProjects) {
+        $body += "Project ID: $($project.id), Name: $($project.name), Last Updated: $($project.lastUpdateTime)`n"
+    }
+    #Send-Email -subject $subject -body $body
+    $send = Send-MailMessage -To $emailTo -SmtpServer $smtpServer -From $emailFrom -Subject $Subject -Body $body
+    Write-Host "The email has been sent: $send"
+}
+Write-Output "The script has checked for renamed Azure DevOps projects and sent an email if any were found."
